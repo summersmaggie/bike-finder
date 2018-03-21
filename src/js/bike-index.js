14adff1936066e7a7b7479b60c31a7d710be9e11
Hannah Lee and Maggie Summers <maggie@>
2018-03-21 16:39:00 -0700

    refactor backend logic to displayBikes.

--- src/js/bike-index.js
+++ src/js/bike-index.js
@@ -1,26 +1,34 @@
 export class bikeAPI {
-  constructor(name, location, displaySuccess) {
+  constructor(name, location, displayBikes) {
     this.name = name;
     this.location = location;
+    this.displayBikes = displayBikes;
+    // this.displayManufacturer = displayManufacturer;
+    // this.displayColor = displayColor;
+    // this.displayYear = displayYear;
   }
 
-  getBikes(name, location) {
+  getBikes(name, location, displayBikes) {
     const bikeKey = process.env.BIKE_INDEX_API_KEY;
 
     $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen&api_key=${bikeKey}&stolenness=proximity`).then(function(response) {
       $('.show-text').text(`The following bikes have been stolen in your area, ${name}:`);
         if (response.bikes.length == 0) {
           $('.stolen-bikes').text("Congratulations, there are no stolen bikes in your area!");
+        // this.displayBikes(response);
         } else {
           for(let i = 0; i < response.bikes.length; i++) {
           let bikeTitle = response.bikes[i].title;
           let bikeSerial = response.bikes[i].serial;
-          $('.stolen-bikes').append(" " + '<li>' + bikeTitle +', ' + '<em>'+ bikeSerial + '</em>' + '</li>');
+          displayBikes(bikeTitle, bikeSerial);
+          // $('.stolen-bikes').append(" " + '<li>' + bikeTitle +', ' + '<em>'+ bikeSerial + '</em>' + '</li>');
         }
+
       }
       }).fail(function(error) {
         $('.api-error').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
       });
+
     }
 
   getManufacturer(location) {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
 import { bikeAPI } from './js/bike-index.js';
 import './styles.css' ;
 
+const displayBikes = function(title, serial) {
+  $('.stolen-bikes').append(`<li>${title} ${serial}</li>`);
+}
+
+// const displayManufacturer = function(title, serial) {
+//   $('#greatDisplay').append(`<div>${title}${serial}</div>`);
+// }
+//
+// const displayColor = function(title, serial) {
+//   $('#greatDisplay').append(`<div>${title}${serial}</div>`);
+// }
+//
+// const displayYear = function(title, serial) {
+//   $('#greatDisplay').append(`<div>${title}${serial}</div>`);
+// }
+
 $(document).ready(function() {
   $('#cyclist-form').submit(function(event) {
     event.preventDefault();
@@ -9,8 +25,8 @@ $(document).ready(function() {
     const location = $("#location").val();
     $("#cyclist-form").hide();
 
-    let myBikeAPI = new bikeAPI(name, location);
-    myBikeAPI.getBikes(name, location);
+    let myBikeAPI = new bikeAPI(name, location, displayBikes);
+    myBikeAPI.getBikes(name, location, displayBikes);
     $("#search").show();
 
     $("#manufacturer").click(function() {
@@ -35,8 +51,3 @@ $(document).ready(function() {
     });
   });
 });
-
-let displaySuccess = function(response) {
-
-
-}
