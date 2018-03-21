d854b2cfd5ade704d1a92ec0566b7867e6b00601
Hannah Lee and Maggie Summers <maggie@>
2018-03-21 11:49:39 -0700

    add google fonts.

--- src/js/bike-index-api.js
+++ src/js/bike-index-api.js
@@ -3,7 +3,6 @@ export function getBikes(location, name) {
 
   $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen&api_key=${bikeKey}&stolenness=proximity`).then(function(response) {
     $('.show-text').text(`The following bikes have been stolen in your area, ${name}:`);
-
       if (response.bikes.length == 0) {
         $('.stolen-bikes').text(`Congratulations, there are no stolen bikes in your area!`);
       } else {
